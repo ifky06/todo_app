be3b17ddb24f8c1664d66e5ab3dc7135fb066947
Ahmad Rifki Fauzi <ahmadrifki3403@gmail.com>
2023-09-02 17:07:42 +0700

    first commit

---
+++ lib/constants/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const Color tdRed = Color(0xFFC71B29);
+const Color tdBlue = Color(0xFF457B9D);
+
+const Color tdBlack = Color(0xFF000000);
+const Color tdGray = Color(0xFF868686);
+
+const Color tdBGColor = Color(0xFFF5F5F5);
