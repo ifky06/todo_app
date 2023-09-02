f42a5a33c4fc2a1f15646123c6b4ffc88c3e9c85
Ahmad Rifki Fauzi <ahmadrifki3403@gmail.com>
2023-09-02 17:54:30 +0700

    CreateAppBar

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app/constants/colors.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
@@ -7,11 +8,34 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Todo App'),
-      ),
+      appBar: _buildAppBar(),
       body: const Center(
         child: Text('Hello World'),
+      ),
+    );
+  }
+
+  AppBar _buildAppBar() {
+    return AppBar(
+      backgroundColor: tdBGColor,
+      elevation: 0,
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Icon(
+            Icons.menu,
+            color: tdBlack,
+            size: 30,
+          ),
+          Container(
+            height: 40,
+            width: 40,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(50),
+              child: Image.asset('assets/images/avatar2.png'),
+            ),
+          )
+        ],
       ),
     );
   }
